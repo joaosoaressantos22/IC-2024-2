dfe7c67ccf3a6110c31f0f4073d81b4200e4feb1
Joao <jotavesantos1@gmail.com>
2024-10-30 19:29:26 -0300

    Calculo do M feito, preciso ajustar calculo dos alfas

--- code/adjacency_tensor.py
+++ code/adjacency_tensor.py
@@ -1,30 +1,34 @@
-#!/usr/bin/env python3
- 
 
 from matplotlib import pyplot as plt
+from itertools import permutations
+from itertools import product
 import numpy as np
 
-x = np.array([
-    [1,2,3],
-    [4,5,6],
-    [4,5,-1]
-])
-# #2x(2x3)
-# tensor = np.array([
-#     [[1,2,3],
-#      [4,5,6]],
-#     [[7,8,9],
-#      [10,11,12]]
-# ])
-# print(type(x))
-# print(x.shape)
-# print(x.ndim)
-# print(x)
-# print(type(tensor))
-# print(tensor.shape)
-# print(tensor.ndim)
-# print(tensor)
+def calculate_m(vetores):
+    max = 0
+    for n in vetores:
+        if (len(n) > max):
+            max = len(n)
+    return max
 
-plt.imshow(x, cmap='Blues')
-plt.colorbar()
-plt.show()
+def calculate_alpha(vetores, m):
+    pos = 0
+    values = [None]*len(vetores) #Copia os valores do tensor de vetores em uma lista
+    for n in vetores:
+        if (m == len(n)):
+            values[pos] = len(list(permutations(n)))
+        else:
+            values[pos] = (len(list(product(n, repeat=m))) - len(n)) #Ainda a trabalhar quando o M é igual a 4 com 3 está dando o dobro
+        pos+= 1
+    return values
+    
+v1 = np.array([1,2])
+v2 = np.array([1,2])
+v3 = np.array([1, 3, 4, 5])
+v4 = np.array([1, 2, 3])
+v5 = np.array([1, 2, 3])
+vetores = [v1, v2, v3, v4, v5]
+m = calculate_m(vetores)
+alfas = calculate_alpha(vetores, m)
+for i in range(len(alfas)):
+    print(alfas[i])
