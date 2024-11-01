7dc0b528ed521ef9e28405ebaff884c9dd1bdf34
Joao <jotavesantos1@gmail.com>
2024-11-01 18:57:41 -0300

    Calculo dos alfas e os valores 50%

--- code/adjacency_tensor.py
+++ code/adjacency_tensor.py
@@ -1,7 +1,6 @@
 
 from matplotlib import pyplot as plt
-from itertools import permutations
-from itertools import product
+from itertools import permutations, product, combinations_with_replacement
 import numpy as np
 
 def calculate_m(vetores):
@@ -12,23 +11,35 @@ def calculate_m(vetores):
     return max
 
 def calculate_alpha(vetores, m):
+    print(m)
     pos = 0
-    values = [None]*len(vetores) #Copia os valores do tensor de vetores em uma lista
+    values = [None]*len(vetores)
     for n in vetores:
         if (m == len(n)):
-            values[pos] = len(list(permutations(n)))
+            values[pos] = len(list(permutations(n))) 
+            print_permutations(list(permutations(n))) 
         else:
-            values[pos] = (len(list(product(n, repeat=m))) - len(n)) #Ainda a trabalhar quando o M é igual a 4 com 3 está dando o dobro
+            newArray = n
+            for j in range((m - len(n))):
+                newArray = np.append(newArray, newArray[j])
+            #print(len(list(permutations(newArray))))
+            values[pos] = len(list(permutations(newArray)))
+            print_permutations(list(permutations(newArray))) 
+            print("SAIU! {}\n")
+            print(values[pos])
         pos+= 1
     return values
+
+
+def calculate_permutations(*lista, m):
+    values = [None]*m
     
-v1 = np.array([1,2])
-v2 = np.array([1,2])
-v3 = np.array([1, 3, 4, 5])
-v4 = np.array([1, 2, 3])
-v5 = np.array([1, 2, 3])
-vetores = [v1, v2, v3, v4, v5]
+def print_permutations(p):
+    for i in list(p):
+        print(i)
+
+v1 = np.array([1,2, 7])
+v4 = np.array([3, 6, 5, 4])
+vetores = [v1, v4]
 m = calculate_m(vetores)
 alfas = calculate_alpha(vetores, m)
-for i in range(len(alfas)):
-    print(alfas[i])
